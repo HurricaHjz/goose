5854ec711b058a2ef97d0a4d194e1f050ef80bfe
Jerry Zhang <u7305891@anu.edu.au>
2024-04-03 00:46:05 +1100

    update experiments

---
+++ CMakeFiles/search.dir/DependInfo.cmake
@@ -0,0 +1,81 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/action.cc" "CMakeFiles/search.dir/action.cc.o" "gcc" "CMakeFiles/search.dir/action.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/action_schema.cc" "CMakeFiles/search.dir/action_schema.cc.o" "gcc" "CMakeFiles/search.dir/action_schema.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/database/hash_join.cc" "CMakeFiles/search.dir/database/hash_join.cc.o" "gcc" "CMakeFiles/search.dir/database/hash_join.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/database/hash_semi_join.cc" "CMakeFiles/search.dir/database/hash_semi_join.cc.o" "gcc" "CMakeFiles/search.dir/database/hash_semi_join.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/database/join.cc" "CMakeFiles/search.dir/database/join.cc.o" "gcc" "CMakeFiles/search.dir/database/join.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/database/project.cc" "CMakeFiles/search.dir/database/project.cc.o" "gcc" "CMakeFiles/search.dir/database/project.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/database/semi_join.cc" "CMakeFiles/search.dir/database/semi_join.cc.o" "gcc" "CMakeFiles/search.dir/database/semi_join.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/database/table.cc" "CMakeFiles/search.dir/database/table.cc.o" "gcc" "CMakeFiles/search.dir/database/table.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/database/utils.cc" "CMakeFiles/search.dir/database/utils.cc.o" "gcc" "CMakeFiles/search.dir/database/utils.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/datalog/datalog.cc" "CMakeFiles/search.dir/datalog/datalog.cc.o" "gcc" "CMakeFiles/search.dir/datalog/datalog.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/datalog/datalog_atom.cc" "CMakeFiles/search.dir/datalog/datalog_atom.cc.o" "gcc" "CMakeFiles/search.dir/datalog/datalog_atom.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/datalog/datalog_fact.cc" "CMakeFiles/search.dir/datalog/datalog_fact.cc.o" "gcc" "CMakeFiles/search.dir/datalog/datalog_fact.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/datalog/grounder/weighted_grounder.cc" "CMakeFiles/search.dir/datalog/grounder/weighted_grounder.cc.o" "gcc" "CMakeFiles/search.dir/datalog/grounder/weighted_grounder.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/datalog/object.cc" "CMakeFiles/search.dir/datalog/object.cc.o" "gcc" "CMakeFiles/search.dir/datalog/object.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/datalog/rule_matcher.cc" "CMakeFiles/search.dir/datalog/rule_matcher.cc.o" "gcc" "CMakeFiles/search.dir/datalog/rule_matcher.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/datalog/rules/rule_base.cc" "CMakeFiles/search.dir/datalog/rules/rule_base.cc.o" "gcc" "CMakeFiles/search.dir/datalog/rules/rule_base.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/hash_structures.cc" "CMakeFiles/search.dir/hash_structures.cc.o" "gcc" "CMakeFiles/search.dir/hash_structures.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/heuristics/add_heuristic.cc" "CMakeFiles/search.dir/heuristics/add_heuristic.cc.o" "gcc" "CMakeFiles/search.dir/heuristics/add_heuristic.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/heuristics/ff_heuristic.cc" "CMakeFiles/search.dir/heuristics/ff_heuristic.cc.o" "gcc" "CMakeFiles/search.dir/heuristics/ff_heuristic.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/heuristics/goalcount.cc" "CMakeFiles/search.dir/heuristics/goalcount.cc.o" "gcc" "CMakeFiles/search.dir/heuristics/goalcount.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/heuristics/goose_heuristic.cc" "CMakeFiles/search.dir/heuristics/goose_heuristic.cc.o" "gcc" "CMakeFiles/search.dir/heuristics/goose_heuristic.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/heuristics/heuristic_factory.cc" "CMakeFiles/search.dir/heuristics/heuristic_factory.cc.o" "gcc" "CMakeFiles/search.dir/heuristics/heuristic_factory.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/heuristics/hmax_heuristic.cc" "CMakeFiles/search.dir/heuristics/hmax_heuristic.cc.o" "gcc" "CMakeFiles/search.dir/heuristics/hmax_heuristic.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/heuristics/perfect_heuristic.cc" "CMakeFiles/search.dir/heuristics/perfect_heuristic.cc.o" "gcc" "CMakeFiles/search.dir/heuristics/perfect_heuristic.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/heuristics/rff_heuristic.cc" "CMakeFiles/search.dir/heuristics/rff_heuristic.cc.o" "gcc" "CMakeFiles/search.dir/heuristics/rff_heuristic.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/heuristics/utils.cc" "CMakeFiles/search.dir/heuristics/utils.cc.o" "gcc" "CMakeFiles/search.dir/heuristics/utils.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/main.cc" "CMakeFiles/search.dir/main.cc.o" "gcc" "CMakeFiles/search.dir/main.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/novelty/standard_novelty.cc" "CMakeFiles/search.dir/novelty/standard_novelty.cc.o" "gcc" "CMakeFiles/search.dir/novelty/standard_novelty.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/parser.cc" "CMakeFiles/search.dir/parser.cc.o" "gcc" "CMakeFiles/search.dir/parser.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/plan_manager.cc" "CMakeFiles/search.dir/plan_manager.cc.o" "gcc" "CMakeFiles/search.dir/plan_manager.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/predicate.cc" "CMakeFiles/search.dir/predicate.cc.o" "gcc" "CMakeFiles/search.dir/predicate.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/alternated_bfws.cc" "CMakeFiles/search.dir/search_engines/alternated_bfws.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/alternated_bfws.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/astar_search.cc" "CMakeFiles/search.dir/search_engines/astar_search.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/astar_search.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/batch_lazy_search.cc" "CMakeFiles/search.dir/search_engines/batch_lazy_search.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/batch_lazy_search.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/breadth_first_search.cc" "CMakeFiles/search.dir/search_engines/breadth_first_search.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/breadth_first_search.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/breadth_first_width_search.cc" "CMakeFiles/search.dir/search_engines/breadth_first_width_search.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/breadth_first_width_search.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/dual_queue_bfws.cc" "CMakeFiles/search.dir/search_engines/dual_queue_bfws.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/dual_queue_bfws.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/greedy_batch_best_first_search.cc" "CMakeFiles/search.dir/search_engines/greedy_batch_best_first_search.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/greedy_batch_best_first_search.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/greedy_best_first_search.cc" "CMakeFiles/search.dir/search_engines/greedy_best_first_search.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/greedy_best_first_search.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/lazy_search.cc" "CMakeFiles/search.dir/search_engines/lazy_search.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/lazy_search.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/nodes.cc" "CMakeFiles/search.dir/search_engines/nodes.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/nodes.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/perfect.cc" "CMakeFiles/search.dir/search_engines/perfect.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/perfect.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/search.cc" "CMakeFiles/search.dir/search_engines/search.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/search.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/search_factory.cc" "CMakeFiles/search.dir/search_engines/search_factory.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/search_factory.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/search_space.cc" "CMakeFiles/search.dir/search_engines/search_space.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/search_space.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_engines/utils.cc" "CMakeFiles/search.dir/search_engines/utils.cc.o" "gcc" "CMakeFiles/search.dir/search_engines/utils.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/search_statistics.cc" "CMakeFiles/search.dir/search_statistics.cc.o" "gcc" "CMakeFiles/search.dir/search_statistics.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/states/extensional_states.cc" "CMakeFiles/search.dir/states/extensional_states.cc.o" "gcc" "CMakeFiles/search.dir/states/extensional_states.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/states/sparse_states.cc" "CMakeFiles/search.dir/states/sparse_states.cc.o" "gcc" "CMakeFiles/search.dir/states/sparse_states.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/states/state.cc" "CMakeFiles/search.dir/states/state.cc.o" "gcc" "CMakeFiles/search.dir/states/state.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/successor_generators/full_reducer_successor_generator.cc" "CMakeFiles/search.dir/successor_generators/full_reducer_successor_generator.cc.o" "gcc" "CMakeFiles/search.dir/successor_generators/full_reducer_successor_generator.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/successor_generators/generic_join_successor.cc" "CMakeFiles/search.dir/successor_generators/generic_join_successor.cc.o" "gcc" "CMakeFiles/search.dir/successor_generators/generic_join_successor.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/successor_generators/ordered_join_successor.cc" "CMakeFiles/search.dir/successor_generators/ordered_join_successor.cc.o" "gcc" "CMakeFiles/search.dir/successor_generators/ordered_join_successor.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/successor_generators/random_successor.cc" "CMakeFiles/search.dir/successor_generators/random_successor.cc.o" "gcc" "CMakeFiles/search.dir/successor_generators/random_successor.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/successor_generators/successor_generator_factory.cc" "CMakeFiles/search.dir/successor_generators/successor_generator_factory.cc.o" "gcc" "CMakeFiles/search.dir/successor_generators/successor_generator_factory.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/successor_generators/yannakakis.cc" "CMakeFiles/search.dir/successor_generators/yannakakis.cc.o" "gcc" "CMakeFiles/search.dir/successor_generators/yannakakis.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/task.cc" "CMakeFiles/search.dir/task.cc.o" "gcc" "CMakeFiles/search.dir/task.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/utils.cc" "CMakeFiles/search.dir/utils.cc.o" "gcc" "CMakeFiles/search.dir/utils.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/utils/logging.cc" "CMakeFiles/search.dir/utils/logging.cc.o" "gcc" "CMakeFiles/search.dir/utils/logging.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/utils/system.cc" "CMakeFiles/search.dir/utils/system.cc.o" "gcc" "CMakeFiles/search.dir/utils/system.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/utils/system_unix.cc" "CMakeFiles/search.dir/utils/system_unix.cc.o" "gcc" "CMakeFiles/search.dir/utils/system_unix.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/utils/system_windows.cc" "CMakeFiles/search.dir/utils/system_windows.cc.o" "gcc" "CMakeFiles/search.dir/utils/system_windows.cc.o.d"
+  "/home/moss/COMP4550/goose/planners/powerlifted/src/search/utils/timer.cc" "CMakeFiles/search.dir/utils/timer.cc.o" "gcc" "CMakeFiles/search.dir/utils/timer.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
